082c7680c238b0daeaccd12631a669224f2bf0e1
Bunny <Bunny>
2026-06-23 23:04:09 +0800

    Update deprecated apple-mobile-web-app-capable meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="th" data-scroll-behavior="smooth">
       <head>
         {isMobile && <link rel="manifest" href="/manifest.json" />}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="SOG Gang" />
       </head>
