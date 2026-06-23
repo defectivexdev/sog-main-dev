6f99c252f86c1679341a829b58ea444b989d82d5
Bunny <Bunny>
2026-06-23 21:24:46 +0800

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -24,8 +24,6 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
 });
 
 export default withPWA(nextConfig);
