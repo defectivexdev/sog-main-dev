8448722afb8ba0074d578049527521d77300d778
Bunny <Bunny>
2026-06-27 01:58:48 +0800

    fix files

--- find_link_a2.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const content = fs.readFileSync("src/components/Navbar.tsx", "utf8");
-const regex = /<Link[^>]*>[\s\S]*?<a[^>]*>([\s\S]*?)<\/a>[\s\S]*?<\/Link>/gi;
-let match;
-while ((match = regex.exec(content)) !== null) {
-  console.log("Found at index:", match.index);
-  console.log(match[0]);
-}
-
